7568771bdfb86ff3d62519772c323805133d1a7c
Thomas Gueutal <thomas.gueutal@student.uantwerpen.be>
2023-04-25 20:11:35 +0200

    Add WIP class generates output files.
    The new class calls the aggregator class
     to produce aggregate results and writes
     those values to an output file. This
     functionality should be extended or mod-
     ified whenever more or different data
     needds to be added to the results.

---
+++ src/Output/SimulationDataPersistor.py
@@ -0,0 +1,31 @@
+import json
+import os
+
+from ..Logic.DataStore import DataStore
+from .SimulationDataAggregator import SimulationDataAggregator
+
+
+class SimulationDataPersistor(object):
+    """This class extracts useful information from a simulation's
+    datastore and then persists it into an output file.
+    """
+    def persist(self, datastore: DataStore, output_path: str) -> None:
+        """Aggregate the simulation data and persist it to an output file.
+
+        :param datastore: The data store to persist
+        :param output_path: The file to persist aggregated results to
+        """
+        output_dir: str = os.path.dirname(output_path)
+        assert os.path.isdir(output_dir), "The output directory does not exist"
+
+        # Setup
+        aggregator: SimulationDataAggregator = SimulationDataAggregator()
+        output: dict = dict()
+
+        # Generate output
+        output["organisms"] = aggregator.aggregateOrganisms(datastore)
+        output["vegetation"] = aggregator.aggregateVegetation(datastore)
+
+        # Persist results
+        with open(output_path, "w") as of:
+            json.dump(output, of)
