f5e84f51364584541437a1e25b9b2d93c4f733f3
Thomas Gueutal <GueutalThomas@outlook.com>
2023-04-25 19:29:58 +0200

    Modify project structure.
    The project structure is suggest is as follows:
     * src/ : All source files related to the simulation code itself
      ==> input files, output files and the main.py file are located
      outside this dir
     * src/UI/ : All UI related files --> the UI logic is separated
      to reliably separate logic from visualization code
     * src/Logic/ : The actual logic behind the simulation, such as
      + animal behavior
      + events
      + natural laws
     * src/Output/ : The serializatio, deserialization and data
      aggregation logic; the logic to process the results of the
      simulation and write it to output files
    
    The Entities/ dir has consequently been moved to .
    
    The file sim.py has been left as is for now, but is due to change
     in a following commit.

---
+++ src/Logic/Entities/vegetation.py
@@ -0,0 +1,5 @@
+class Vegetation:
+    def __init__(self, v_name: str, v_age: int, v_id: int):
+        self.name: str = v_name
+        self.age: int = v_age #TODO: Don't work with age?
+        self.id: int = v_id
