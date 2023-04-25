bdd00426ca393157b07a586a8fe70126b3443890
Thomas Gueutal <thomas.gueutal@student.uantwerpen.be>
2023-04-25 20:15:19 +0200

    Rework the original Simulation class.
    The original Simulation class performed
     the data loading itself. This has been
     delegated to the previously added loader
     class.
    
    The Simulation class now provides a basic
     interface for
      1) persisting the current results to disk,
       with its persist() method
      2) running the simulation, wich currently
       just loads data into the datastore, with
       its run() method
    The Simulation.__init__ method has been changed
     to make use of the DataStore

---
+++ src/Logic/Simulation.py
@@ -0,0 +1,29 @@
+from .DataStore import DataStore
+from ..Output.SimulationDataPersistor import SimulationDataPersistor
+from ..Output.SimulationDataLoader import SimulationDataLoader
+
+
+class Simulation(object):
+    def __init__(self, s_path: str) -> None:
+        self.dataStore: DataStore = DataStore(s_path)
+
+    def run(self):
+        """Run the simulation.
+
+        This is the entry point of the simulation.
+        """
+        self._load()
+
+    def persist(self, output_path: str) -> None:
+        """Persist the contents and results of the simulation to an output file.
+
+        :param output_path: The path to write the output to
+        """
+        aggregator: SimulationDataPersistor = SimulationDataPersistor()
+        aggregator.persist(self.dataStore, output_path)
+
+    def _load(self):
+        """Populate the simulation from an input file.
+        """
+        loader: SimulationDataLoader = SimulationDataLoader()
+        loader.load(self.dataStore, self.dataStore.session_path)
