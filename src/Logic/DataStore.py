e4f067726976cc23d8e63124674b63191a53442a
JeffBoermans <Jeff.Boermans@student.uantwerpen.be>
2023-04-26 14:16:50 +0200

    Added the storage of the foodchain

--- src/Logic/DataStore.py
+++ src/Logic/DataStore.py
@@ -7,9 +7,10 @@ from .Entities.vegetation import Vegetation
 
 
 class DataStore(object):
-    """This clas contains the data logic of a simulation.
+    """This class contains the data logic of a simulation.
     """
     def __init__(self, s_path: str):
         self.session_path: str = s_path
         self.organisms: List[Organism] = []
         self.vegetation: List[Vegetation] = []
+        self.foodChain: dict = {}
