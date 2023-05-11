df14e27bb59b1300680fffe70aca9379d20d652e
Thomas Gueutal <thomas.gueutal@student.uantwerpen.be>
2023-05-11 20:14:58 +0200

    Add lifespan type hint.

--- src/Logic/Entities/organism_info.py
+++ src/Logic/Entities/organism_info.py
@@ -1,14 +1,16 @@
+from typing import Tuple
+
+
 class OrganismInfo:
     """Properties specific to a species, to be shared between
     Organism instances.
     """
-    def __init__(self, o_m: int, o_b: int, o_ls, o_mpa: int):
+    def __init__(self, o_m: int, o_b: int, o_ls: Tuple[int, int], o_mpa: int):
         """
         :param o_m: Sexual maturity (days)
         :param o_b: Breeding age (days)
-        :param o_ls: Life span, represented in a list (months)
+        :param o_ls: Life span, represented as a tuple (months)
         :param o_mpa: Start menopause (days)
-        :return:
         """
         self.maturity: int = o_m
         self.breeding: int = o_b
