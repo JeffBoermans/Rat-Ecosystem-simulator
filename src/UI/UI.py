d1215dbf760e2d3fa82ac455ff2040754fcceb65
SanderMar <sander.marinus@student.uantwerpen.be>
2023-05-12 16:39:21 +0200

    Links simple simulation to UI + run UI

--- src/UI/UI.py
+++ src/UI/UI.py
@@ -2,20 +2,23 @@ import dearpygui.dearpygui as dpg
 import dearpygui_ext.logger as logger
 import time
 
+from ..Logic.Simulation import Simulation
+
 
 class dpg_plot():
     def __init__(self, _dpg) -> None:
-        self.time_step = 1
         self.data_x = [0.0]
-        self.data_y = [2.0]
+        self.data_y = [0.0]
         self.x_axis = None
         self.y_axis = None
         self._dpg = _dpg
-        self.updatectr = 0
         self.sec_tick = 0
 
         self.paused = True
         self.prev_update = time.time()
+
+        self.logger = None
+        self.simulation = None
 
     def runSimulation(self, sender, callback):
         if self._dpg.get_value("file_selected") == "":
@@ -26,11 +29,15 @@ class dpg_plot():
         b_virus = self._dpg.get_value("b_virus")
         self.sec_tick = self._dpg.get_value("i_tick")
 
-        print(f"Number of rats: {nr_rats}")
+        self.simulation = Simulation(self._dpg.get_value("file_selected"))
+        self.simulation._load()
+        self.data_y[0] = self.simulation.organism_count()
+
+        print(f"Number of rats: {self.simulation.organism_count()}")
         print(f"Random Fires: {b_fires}")
         print(f"Random Viruses: {b_virus}")
         print(f"Seconds per tick: {self.sec_tick}")
-        logger_ = logger.mvLogger(self._dpg.add_window(
+        self.logger = logger.mvLogger(self._dpg.add_window(
             label="mvLogger", pos=(0, 350), width=350, height=350))
         with self._dpg.window(label="Simulation Window", width=350, height=350) as plot_window:
             with dpg.group(horizontal=True):
@@ -48,15 +55,18 @@ class dpg_plot():
                                       tag="series_tag")
         self._dpg.set_item_pos(plot_window, [0, 0])
         with self._dpg.window(label="Data box", width=350, height=350) as data_window:
-            self._dpg.add_text(f"Ticks: {self.updatectr}", tag="updatectr")
+            self._dpg.add_text(
+                f"Ticks: {self.simulation.day()}", tag="updatectr")
             self._dpg.add_text("Number of rats in existence: ")
             self._dpg.add_text("Number of rats died: ")
             self._dpg.add_text("Number of random occurences happened: ")
         self._dpg.set_item_pos(data_window, [350, 0])
-        logger_.log("Rat 1 starved to death")
+        # Logging example
+        # self.logger.log("Rat 1 starved to death")
 
     def fileCallback(self, app_data, sender):
-        self._dpg.set_value("file_selected", f"{sender['selections'][sender['file_name']]}")
+        self._dpg.set_value(
+            "file_selected", f"{sender['selections'][sender['file_name']]}")
 
     def setUpSimulation(self):
         self._dpg.create_context()
@@ -77,13 +87,15 @@ class dpg_plot():
             self._dpg.add_text("=================")
             self._dpg.add_text("Add a session file (.json)")
             self._dpg.add_text("-----------------")
-            with self._dpg.file_dialog(directory_selector=False, show=False, id="file_dialog_id", width=700 ,height=400, 
+            with self._dpg.file_dialog(directory_selector=False, show=False, id="file_dialog_id", width=700, height=400,
                                        callback=self.fileCallback):
                 self._dpg.add_file_extension(".json")
-            self._dpg.add_button(label="File Selector", callback=lambda: dpg.show_item("file_dialog_id"))
+            self._dpg.add_button(
+                label="File Selector", callback=lambda: dpg.show_item("file_dialog_id"))
             self._dpg.add_text("", tag="file_selected")
             self._dpg.add_text("=================")
-            self._dpg.add_button(label="Start Simulation", callback=self.runSimulation)
+            self._dpg.add_button(label="Start Simulation",
+                                 callback=self.runSimulation)
         self._dpg.setup_dearpygui()
         self._dpg.show_viewport()
         self._dpg.set_primary_window("primary", True)
@@ -107,21 +119,25 @@ class dpg_plot():
         self._dpg.destroy_context()
 
     def update_data(self):
-        self.updatectr += 1
+        if self.simulation is None:
+            return
         self._dpg.set_value(
-            "updatectr", f"Time steps processed: {self.updatectr}")
-        # Get new data sample. Note we need both x and y values
+            "updatectr", f"Time steps processed: {self.simulation.day()}")
+        # Advance the time in the simulation
+        logs = self.simulation.simulate()
+        # Get new data from the simulation. Note we need both x and y values
         # if we want a meaningful axis unit.
-        self.data_x.append(self.time_step)
-        self.data_y.append(self.data_y[self.time_step-1]*2)
+        self.data_x.append(self.simulation.day())
+        self.data_y.append(self.simulation.organism_count())
+
+        for log in logs:
+            self.logger.log(log)
 
         # set the series x and y to the last nsamples
         self._dpg.set_value(
             'series_tag', [list(self.data_x), list(self.data_y)])
         dpg.fit_axis_data('x_axis')
         dpg.fit_axis_data('y_axis')
-
-        self.time_step = self.time_step+1
 
 
 if __name__ == '__main__':
