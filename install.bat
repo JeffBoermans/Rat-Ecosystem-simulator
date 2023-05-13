f41cab138f8701af3b352d17cc371e52031f350e
Thomas Gueutal <thomas.gueutal@student.uantwerpen.be>
2023-05-13 16:41:33 +0200

    Attempt to fix bat files syntax errors.

--- install.bat
+++ install.bat
@@ -2,9 +2,9 @@
 
 REM Setup venv
 echo "[Install] Setup python virtual env"
-python3 "-m" "venv" "venv"
-. "venv\bin\activate"
+python -m venv venv
+venv\bin\activate
 
 REM Install reqs
 echo "[Install] Install dependencies"
-pip "install" "-r" "requirements.txt"
+pip install -r requirements.txt
