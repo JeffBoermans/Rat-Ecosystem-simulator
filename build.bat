8738fe37ad2105d0b1bfc142caf6a515aef6e70f
Thomas Gueutal <thomas.gueutal@student.uantwerpen.be>
2023-05-13 17:38:27 +0200

    Another .bat venv activate fix attempt.

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@ SET DIR=build
 
 REM Setup build output location
 echo "[Build] Setup build dir"
-rem venv/Scripts/activate
+.\venv\Scripts\activate
 if not exist %DIR% mkdir %DIR%
 
 REM Build the project into an executable
