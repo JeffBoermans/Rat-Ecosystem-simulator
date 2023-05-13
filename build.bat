25513dbc788e8b5f53cc5868f7b3c0dc3069136a
Thomas Gueutal <thomas.gueutal@student.uantwerpen.be>
2023-05-13 17:24:53 +0200

    Use alternate if syntax in .bat.

--- build.bat
+++ build.bat
@@ -3,9 +3,7 @@ SET DIR=build
 REM Setup build output location
 echo "[Build] Setup build dir"
 venv/Scripts/activate
-if not exist %DIR% (
-    mkdir %DIR%
-)
+if not exist %DIR% mkdir %DIR%
 
 REM Build the project into an executable
 echo "[Build] Build executable at %DIR%"
