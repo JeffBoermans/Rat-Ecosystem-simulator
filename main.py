2d1e3c696e22e9bb6fe889aab63200a927d91ef9
JeffBoermans <Jeff.Boermans@student.uantwerpen.be>
2023-05-18 13:51:15 +0200

    Changed name of UI object

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import dearpygui.dearpygui as dpg
 from src.UI.UI import UI
 
-#Test push for circleCI
+
 def main():
     """The main entrypoint of the simulation application.
     """
-    plot = UI(dpg)
-    plot.setUpSimulation()
-    plot.run()
+    sim = UI(dpg)
+    sim.setUpSimulation()
+    sim.run()
 
 
 if __name__ == '__main__':
