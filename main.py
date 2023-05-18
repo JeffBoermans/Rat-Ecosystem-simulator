6f96c93a0af8a19e06dcc8c74edfdd46f8beff6e
Thomas Gueutal <thomas.gueutal@student.uantwerpen.be>
2023-05-18 15:45:24 +0200

    Register the Foraging extension.

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import dearpygui.dearpygui as dpg
+
 from src.UI.UI import UI
+from src import FullExtensions
 
 #Test push for circleCI
 def main():
     """The main entrypoint of the simulation application.
     """
-    plot = UI(dpg)
+    plot = UI(dpg, extensions=[
+        FullExtensions.ForagingExtension.ForagingExtensionFull()
+    ])
     plot.setUpSimulation()
     plot.run()
 
