75d80318d74be504b92fa508e1fc86e16f2e2696
Sander Marinus <sander.marinus@student.uantwerpen.be>
2023-05-18 14:32:12 +0200

    Start cleanup UI code

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import dearpygui.dearpygui as dpg
 from src.UI.UI import UI
 
-#Test push for circleCI
+
 def main():
     """The main entrypoint of the simulation application.
     """
     plot = UI(dpg)
-    plot.setUpSimulation()
+    plot.setup_main_window()
     plot.run()
 
 
