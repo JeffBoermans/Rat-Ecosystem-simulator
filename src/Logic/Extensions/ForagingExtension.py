353acca330f32a687398134f5c96043e9b93551b
Thomas Gueutal <thomas.gueutal@student.uantwerpen.be>
2023-05-19 13:08:32 +0200

    Add foraging extension parameters based on prev. commit.

--- src/Logic/Extensions/ForagingExtension.py
+++ src/Logic/Extensions/ForagingExtension.py
@@ -1,12 +1,13 @@
 import random
 
-from typing import Dict, List, Tuple
+from typing import Any, Dict, List, Tuple, Union
 
 from .SimulationExtension import SimulationMortalityExtension
 from ..DataStore import DataStore
 from ..Entities.entity import Entity
 from ..Entities.organism import Organism
 from ..Entities.vegetation import MonoVegetationCluster
+from ..exceptions import MissingOrganismProperty, MissingVegetationProperty
 
 
 class ForagingExtension(SimulationMortalityExtension):
@@ -14,6 +15,12 @@ class ForagingExtension(SimulationMortalityExtension):
         super().__init__(extension_name)
         self.organism_foraging_info: Dict[Organism, Tuple[MonoVegetationCluster, int]] = dict()
         self.cluster_organism_populations: Dict[MonoVegetationCluster, int] = dict()
+
+        # Extension input properties
+        self.PROP_ORG_DAILY_ENERGY_CONSUMPTION   = "daily-energy-consumption"
+        self.PROP_ORG_ENERGY_FORAGING_AMOUNT     = "energy-foraging-amount"
+        self.PROP_ORG_DAYS_ENERGY_STOCKPILE_MAX  = "days-energy-stockpile-max"
+        self.PROP_VEG_FORAGING_DIFFICULTY_FACTOR = "foraging-difficulty-factor"
 
     def get_cluster_organism_population(self, cluster: MonoVegetationCluster) -> int:
         """Get the organism population for the cluster."""
@@ -24,7 +31,6 @@ class ForagingExtension(SimulationMortalityExtension):
         if len(datastore.vegetation) == 0:
             return
 
-        daily_energy_upkeep: int = 1
         for organism in datastore.organisms:
             # Add organism to cluster
             if organism not in self.organism_foraging_info:
@@ -35,18 +41,29 @@ class ForagingExtension(SimulationMortalityExtension):
             # Organism is on cluster, get its foraging info
             else:
                 cluster, energy = self.organism_foraging_info[organism]
-            # TODO REMOVE HARDCODED LIMIT
-            # TODO REMOVE HARDCODED LIMIT
-            # TODO REMOVE HARDCODED LIMIT
-            surplus_energy_hardcap: int = 365
-            to_forage_energy: int = 1 + (energy <= surplus_energy_hardcap)
-            to_forage_energy = cluster.forage_energy(to_forage_energy)
-            new_info = (cluster, max(0, energy + to_forage_energy - daily_energy_upkeep))
+
+            daily_consumption: int | None = organism.organismInfo.get_extension_property(self.PROP_ORG_DAILY_ENERGY_CONSUMPTION)
+            forage_amount: int | None = organism.organismInfo.get_extension_property(self.PROP_ORG_ENERGY_FORAGING_AMOUNT)
+            # The amount of days worth of energy upkeep the organism is
+            # allowed to stockpile
+            energy_stockpile_days: int | None = organism.organismInfo.get_extension_property(self.PROP_ORG_DAYS_ENERGY_STOCKPILE_MAX)
+            if daily_consumption is None:
+                raise MissingOrganismProperty(f"An organism's organism info is missing a required extension key: {self.PROP_ORG_DAILY_ENERGY_CONSUMPTION}")
+            if forage_amount is None:
+                raise MissingOrganismProperty(f"An organism's organism info is missing a required extension key: {self.PROP_ORG_ENERGY_FORAGING_AMOUNT}")
+            if energy_stockpile_days is None:
+                raise MissingOrganismProperty(f"An organism's organism info is missing a required extension key: {self.PROP_ORG_DAYS_ENERGY_STOCKPILE_MAX}")
+            satiation_hardcap: int = energy_stockpile_days * daily_consumption
+            room_in_stomach: int = satiation_hardcap - energy # The amount of energy the organism can still stockpile
+            to_forage_energy: int = daily_consumption + min(forage_amount, room_in_stomach)
+            foraged_energy = cluster.forage_energy(to_forage_energy)
+            new_info = (cluster, max(0, energy + foraged_energy - daily_consumption))
             self.organism_foraging_info[organism] = new_info
 
     def notify_organism_death(self, organism: Organism) -> None:
-        cluster, _ = self.organism_foraging_info.pop(organism)
-        self._decr_cluster_organism_count(cluster)
+        if organism in self.organism_foraging_info:
+            cluster, _ = self.organism_foraging_info.pop(organism)
+            self._decr_cluster_organism_count(cluster)
 
     def should_die(self, organism: Entity, datastore: DataStore, log: List[str]) -> bool:
         if not isinstance(organism, Organism):
--- src/Logic/exceptions.py
+++ src/Logic/exceptions.py
@@ -1,3 +1,25 @@
-class DuplicateExtension(Exception):
+class ExtensionException(Exception):
+    """An exception occurred related to extensions"""
+    pass
+
+
+class DuplicateExtension(ExtensionException):
     """A simulation detects a duplicate extension"""
+    pass
+
+
+class MissingExtensionProperty(ExtensionException):
+    """The extension could not retrieve a needed property from somewhere."""
+    pass
+
+class MissingOrganismProperty(MissingExtensionProperty):
+    """The extension could not retrieve a required extension property from an
+    organism's extension properties
+    """
+    pass
+
+class MissingVegetationProperty(MissingExtensionProperty):
+    """The extension could not retrieve a required extension property from a
+    vegetation's extensions properties
+    """
     pass
