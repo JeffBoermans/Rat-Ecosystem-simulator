2bc6f65cafd37ba9ec2c80102f49deeafaec6f4b
Thomas Gueutal <thomas.gueutal@student.uantwerpen.be>
2023-05-19 13:05:56 +0200

    Allow OrganisInfo and Vegetation to specify extension props.
    Every non-required keyword argument to the
     constructor of Vegetation or its derivatives
     and OrganismInfo is seen as an extension prop.
     They denote properties that extensions to the
     simulation might use and allow the session files
     to be extended more organically, without having
     to alter the data loader code every time.

--- src/Logic/Entities/organism_info.py
+++ src/Logic/Entities/organism_info.py
@@ -1,8 +1,11 @@
+from typing import Any, Dict, Union
+
+
 class OrganismInfo:
     """Properties specific to a species, to be shared between
     Organism instances.
     """
-    def __init__(self, o_m: int, o_b: int, o_ls: list[int], o_mpa: list[int]):
+    def __init__(self, o_m: int, o_b: int, o_ls: list[int], o_mpa: list[int], **kwargs):
         """
         :param o_m: Sexual maturity (days)
         :param o_b: Breeding age (days)
@@ -18,6 +21,15 @@ class OrganismInfo:
         self.lifespan = o_ls
         self._month_to_days(o_mpa)
         self.menopause = o_mpa
+        self.extension_properties: Dict[str, Any] = { **kwargs }
+        """Any non-required or expected organism info properties get stored for use by extensions."""
+
+    def get_extension_property(self, property_name: str) -> Union[Any, None]:
+        """Get the value of an extension property.
+        
+        :return: The property value, None if the property does not exist
+        """
+        return self.extension_properties.get(property_name, None)
 
     def _month_to_days(self, o: list[int]):
         for i in range(0,len(o)):
